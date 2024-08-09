714fe88148944fe4bd614b5bfd7c32f731bf2b31
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-08-09 16:28:45 +0500

    Add files via upload

---
+++ QR code-Generator/class1.py
@@ -0,0 +1,112 @@
+# print("Hello world")  
+
+#! VARIABLES
+# aqsa = 120
+# print(aqsa)
+#? we can not use num, special character, and space in starting of the name in variables.
+
+#! DATA TYPES 
+#! numbers = integers(int), float, complex
+# a = 12
+# print(type(a)) # int
+
+# a = 12.0
+# print(type(a)) # float
+
+# a = 6/3j
+# print(type(a)) # complex
+
+#! boolean = true, false
+# a = True  #? if i write true or false with small t and f we got error.
+# print(type(a)) #? bool
+
+#! string  = "", '' 
+# a = "1aqsa"
+# print(type(a)) # str
+
+#! TYPE CASTING / TYPE CONVERSION
+#? as following way we can change type of any type.
+# a = "12"
+# print(type(a))
+# b = int(a)
+# print(b,(type(b)))
+
+# a = 0
+# a = bool(a)
+# print(a)
+
+#! INPUT
+#? when u want to sk something from user you have to use input function.
+# name = str(input("what is your name?"))
+# print(type(name))
+
+#? to change line
+# print("im aqsa \nmy age is 20" )
+
+#? formated string
+# name = "aqsa"
+# age = 20
+# print(f"my name is {name} and my age is {age}")
+
+#! ARITHMETIC OPERATORS
+# print(12 + 13)
+# print(1 - 2)
+# print(2 * 2)
+# print(2 / 2)
+# print(2 ** 2) #? power
+# print(2 % 5)  #? reminder
+
+#! COMPARISION OPERATORS
+# print(1 == 4) 
+# print(3 != 5) 
+# print(2 > 4) 
+# print(5 < 3) 
+# print(2 >= 2) 
+# print(3 <= 5) 
+
+#! LOGICAL OPERATORS
+# print(12 < 13 and 34 > 23 and 23 > 1) #? all conditions must be true.
+# print(12 < 13 or 45 > 67 or 12 > 34 ) #? agr ek condition bh true hogi to true print hoga
+# print(not 12 > 56 and 34 > 12 and not 67 < 34) #? condition change hoti khud hi.
+
+#! CONTROL FLOW (IF/ELSE)
+
+# age = int(input("please enter your age"))
+# if age >=18:
+#     print("you can vote")
+# else:
+#     print("you can't vote ")
+
+# item = input("which icecream you have?:")
+# if item == "pista":
+#     print("pack it")
+# elif item == "vanila":
+#     print("pack it")
+# else:
+#     print("ok thanks")
+
+#! ITERATIVE STATEMENTS (LOOPS)
+# for i in range(1,11,1):
+#     print(i)
+#     if i == 5:
+#         break
+
+# a = 0
+# while a < 10:
+#     print(a)
+#     a = a + 1
+#     if a == 5
+#     break
+
+# for i in range(1,11):
+#     if i == 5 or i == 7 or i == 8:
+#         continue
+#     print(i)
+
+
+
+
+
+
+
+
