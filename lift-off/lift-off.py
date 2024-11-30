7e1a4fcf433456742e34dea104c215a23e81974e
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-11-30 20:02:19 +0500

    Add files via upload

---
+++ lift-off/lift-off.py
@@ -0,0 +1,10 @@
+def main():
+    # Countdown from 10 to 1
+    for i in range(10, 0, -1):  # Start from 10, stop before 0, decrement by 1
+        print(i, end=" ")
+    # Print "Liftoff!" after the countdown
+    print("Liftoff!")
+
+# Call the main function
+if __name__ == "__main__":
+    main()
