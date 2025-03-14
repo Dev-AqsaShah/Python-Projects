d15526ecd958121c1362dba46b355e131b170f3b
aqsashah <aqsanor665@gmail.com>
2025-03-14 22:17:20 +0500

    .

---
+++ quiz-app/main.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import random 
+
+st.title(" Quiz Application")
+
+questions = [
+    {
+        "question": "what is the capital of Pakistan?",
+        "option": ["Karachi", "Lahore", "Islamabad", "Peshawar" ],
+        "answer": "Islamabad",
+    },
+    {
+        "question": "Who is the founder of Pakistan?",
+        "options": [
+            "Allama Iqbal",
+            "Liaquat Ali khan",
+            "Muhammad Ali Jinnah",
+            "Benazir Bhutto",
+        ],
+        "answer": "Muhammad Ali Jinnah",
+    },
+    {
+        "question": "What is the currency of Pakistan?",
+        "options": ["Rupee", "Dollar", "Taka", "Riyal"],
+        "answer": "Rupee",
+    },
+    {
+        "question": "Which is the national language of Pakistan?",
+        "options": ["Punjabi", "Urdu", "Sindhi", "Pashto"],
+        "answer": "Urdu",
+    },
+    {
+        "question": "which city is known"
+    }
+]
