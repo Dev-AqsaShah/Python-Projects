af90913820390d5aa494541d4208affe22b6fd34
aqsashah <aqsanor665@gmail.com>
2025-03-22 21:46:46 +0500

    .

---
+++ python-practice/lists.py
@@ -0,0 +1,15 @@
+#marks = [94.4, 87.5, 95.2, 66.4, 45.1]
+#print(marks)
+#print(len(marks))
+#print(marks[0])
+#print(marks[1])
+
+#list slicing
+# marks = [85, 94, 76, 65, 78]
+# print(marks[1:])
+
+list methods
+
+list = [2,1,4]
+
+list.append(4) adds one element at the end [2,1,4,5]
