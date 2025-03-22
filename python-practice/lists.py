9a25aa8d96f0c7169ae1f0e9025b8001da73cafd
aqsashah <aqsanor665@gmail.com>
2025-03-22 22:22:52 +0500

    Update lists.py

--- python-practice/lists.py
+++ python-practice/lists.py
@@ -8,8 +8,26 @@
 # marks = [85, 94, 76, 65, 78]
 # print(marks[1:])
 
-list methods
+# list methods
+# list = [2,1,4]
 
-list = [2,1,4]
+# list.append(4) adds one element at the end [2,1,4,5]
+# list = [2, 1, 3]
+# list.append(4)
+# print(list)
 
-list.append(4) adds one element at the end [2,1,4,5]
+# list = [2, 1,4]
+# print(list.sort())
+# print(list)
+
+# list = [2, 1,4]
+# list.remove(1)
+# print(list)
+
+# list = [2, 1,4]
+# list.pop(2)
+# print(list)
+
+# list. click krn s vs ham suggest kr deta h sare mothods
+
+
