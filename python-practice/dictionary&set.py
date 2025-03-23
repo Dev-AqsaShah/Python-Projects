fad57deda0e73f121bce9cd28a3ce9d88b8de16a
aqsashah <aqsanor665@gmail.com>
2025-03-23 15:20:03 +0500

    list, tuples

---
+++ python-practice/dictionary&set.py
@@ -0,0 +1,16 @@
+# dictionary
+# dictionaries are used to stor data values in key.value pairs
+# they are unordered mutable(changeable) and dont allow duplicate keys
+# ditionary ko hm curly brackets m likhte hn
+# info = {
+#     "key" : "value",
+#     "name" : "aqsa",
+#     "leaning" : "codding",
+#     "age" : 20,
+#     "is_adult" : True,
+#     "marks" :94.4
+# }
+# print(info)
+# is m hm list or tupls ko bhi likh sakt hn
+
+
---
+++ python-practice/lists&tuples.py
@@ -0,0 +1,68 @@
+#marks = [94.4, 87.5, 95.2, 66.4, 45.1]
+#print(marks)
+#print(len(marks))
+#print(marks[0])
+#print(marks[1])
+
+#list slicing
+# marks = [85, 94, 76, 65, 78]
+# print(marks[1:])
+
+# list methods
+# list = [2,1,4]
+
+# list.append(4) adds one element at the end [2,1,4,5]
+# list = [2, 1, 3]
+# list.append(4)
+# print(list)
+
+# list = [2, 1,4]
+# print(list.sort())
+# print(list)
+
+# list = [2, 1,4]
+# list.remove(1)
+# print(list)
+
+# list = [2, 1,4]
+# list.pop(2)
+# print(list)
+
+# list. click krn s vs ham suggest kr deta h sare mothods
+
+# tuples
+# list mutable hoti h mtlb chang hone wali 
+# tuple immutable hote hn mtlb not changable
+# tuple bhi same list ki tarha likht hn bs squar braces ki jagah round backets dete hn.
+# tuple m single value k sath coma lagana zaruri h
+
+
+# practice questions
+# question 1
+# wap to ask the user names of thier 3 favorite movies & stor thm in a list
+
+# movies = []
+# movies.append(input("enter 1st movie: "))
+# movies.append(input("enter 2nd movie:"))
+# movies.append(input("enter 3rd movie:"))
+
+# print(movies)
+
+
+# question 2
+# wap to check if a list contains a palindrome of element. (hint: use copy() method)
+
+# list1 = [1, 2, 1]
+# list2 = [1, 2, 3]
+
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
+
+# if(copy_list1 == list1):
+#     print("palindrome")
+# else:
+#     print("NOT palindrome")
+
+# grade = ["C", "D", "A", "B", "A"]
+# grade.sort()
+# print(grade)
