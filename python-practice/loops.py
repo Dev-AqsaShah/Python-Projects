c7a66eca06cb283d31a59528e28b0e6b7f145631
aqsashah <aqsanor665@gmail.com>
2025-04-18 20:27:58 +0500

    .

---
+++ python-practice/loops.py
@@ -0,0 +1,76 @@
+# count = 1
+# while count<= 5 :
+#     print("aqsa")
+#     count += 1
+# print(count)
+
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i += 5
+    
+# print("end")
+    
+    
+# q1
+# print num from 1 to 100
+# print num from 100 to 1
+# print the multiplication tabl of a num n 
+# print the elements of the following list using a loop
+# search for a number x in this tuple using loop
+
+# i = 1
+# while i >+ 100:  stooping 
+    # print(i)
+    # i += 
+    
+# q3
+# n = int(input("entr num:"))
+# i = 1
+# while i <= 10:
+#     print(n*i)
+#     i += 1
+
+# q4
+# num = [1,4,9,16,25,36,49,64,81,100]
+# heros = ["aqsa", "fasih", "no", "haan"]
+
+# traverse mtlb ek ek 
+# idx = 0
+# while idx < len(heros):
+#     print(heros[idx])
+#     idx += 1
+    
+
+# idx = 0 
+# while idx < len(num):
+#     print(num[idx]) #nums[0], nums[1], nums[2]
+#     idx +- 1
+
+
+num = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 36 )
+x = 36
+
+i = 0
+while i < len(num):
+    if(num[i] == x):
+        print("FOUND at idx", i)
+    else:
+        print("finding..")
+    i += 1
+    
+#! break keyword : used to terminate the loop when encountered
+#! continue : terminates execution in the current iteration & continues execution of the loop with the next iteration
+# i = 0
+# while i <= 5:
+#     if(i == 3):
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+
+#?  for loop 
+#? loops are used for sequential traversal. for traversal for traversing list string tuples 
+
+num = [1, ]
