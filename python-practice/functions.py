49eb5de4394f7841d65dbc58c5fef4f9910b42e3
aqsashah <aqsanor665@gmail.com>
2025-04-28 21:58:53 +0500

    Update functions.py

--- python-practice/functions.py
+++ python-practice/functions.py
@@ -1,4 +1,68 @@
 #! functions in python
 #? block of statements that perform a specific task.
 
-a = 5
+# def calc_sum(a, b):
+#     sum = a + b
+#     print(sum)
+#     return sum
+
+# calc_sum(5, 10)
+
+# a = 5
+# b = 10
+
+# sum = a + b
+# print(sum)
+
+# #avrag of 3 num
+
+# def calc_avg(a,b,c):
+#     sum = a + b + c
+#     avg = sum / 3
+#     print(avg)
+#     return avg
+
+# calc_avg(1,2,3)
+
+#! typs of function
+#? built-in function => print(), len(), type(), range() 
+#? usr defined function
+
+#! default parameters
+#? assigning a default value to parameter, which is used when no argument is passed
+#? default valu last m dete hn 
+
+# def cal_prod(b, a=2):
+#     print(a*b)
+#     return a * b
+
+# cal_prod(1)
+
+#! q1
+#? waf to print the length of list, list is parameter
+
+# cities = ["delhi", "gurgaon", "noida", "karachi", "jamshoro", "hyderabad"]
+# heroes = ["thor", "ironman", "captain america", "spiderman"]
+
+# def print_len(list):
+#     print(len(list))
+
+
+#? q2 write a function to print th elements of a list in a single line. (list is the parameter)
+
+# cities = ["delhi", "gurgaon", "noida", "karachi", "jamshoro", "hyderabad"]
+# heroes = ["thor", "ironman", "captain america", "spiderman"]
+# def print_len(list):
+#     print(len(list))
+    
+# def print_list(list):
+#     for item in list:
+#         print(item, end="")
+        
+        
+# print_list(cities)
+# print()
+
+
+
+
