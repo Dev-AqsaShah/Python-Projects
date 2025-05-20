8da615fd1027faed7bb142fe6170f486fda6887b
aqsashah <aqsanor665@gmail.com>
2025-05-20 12:16:33 +0500

    Create oops.py

---
+++ python-practice/oops.py
@@ -0,0 +1,34 @@
+# oop in python 
+# to map with real world scenarios, we started using objects in code. this is called object oriented programming.
+
+# class & object in python
+# class is a blueprint for creating objects.
+
+# class Student:
+#     name = "aqsa shah"
+    
+    
+# s1 = Student()
+# print(s1.name)
+
+# s2 = Student()
+# print(s2.name)
+
+
+
+# __init__ function
+# constructor
+# all classes have a finction called _init_(), which is always executed when the class is being initiated.
+
+# class Student:
+    
+#     def __init__(self, fullname):
+#         self.name = fullname
+#         print("adding new student in Database.")
+
+# s1 = Student("aqsa")
+# print(s1.name)
+
+
+
+# CLASS & INSTANCE ATTRIBUTES
